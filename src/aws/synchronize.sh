af294c94768c20ce024c52c0761afb79286a3017
Johannes Schüth <j.schueth@gentics.com>
2012-07-31 13:27:25 +0200

    Updated sync. - Added experimental ftp sync for another cdn.

--- src/aws/synchronize.sh
+++ src/aws/synchronize.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-java -classpath $(echo target/jets3t/*.jar | tr ' ' ':')  org.jets3t.apps.synchronize.Synchronize --batch --properties target/aws/aws.s3.synchronize.properties UP alohaeditor target/cdn/*
+java -classpath $(echo target/jets3t/*.jar | tr ' ' ':'):src/s3.config  org.jets3t.apps.synchronize.Synchronize --batch --properties target/aws/aws.s3.synchronize.properties UP alohaeditor target/cdn/*
